ff94d6c23dc266b104c7b6b1f6e77ec955156aa5
admiralcoder <djale2008@gmail.com>
2021-08-28 22:31:54 -0400

    update class 5

--- src/test/java/api_class1/getWorkspaceMemberByWorkspace_05.java
+++ src/test/java/api_class1/getWorkspaceMemberByWorkspace_05.java
@@ -1,6 +1,5 @@
 package api_class1;
 
-import com.sun.xml.bind.v2.model.core.TypeRef;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -8,10 +7,11 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
+import static org.apache.http.HttpStatus.SC_OK;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.json.JSONObject;
 
 public class getWorkspaceMemberByWorkspace_05 {
 
@@ -23,47 +23,40 @@ public class getWorkspaceMemberByWorkspace_05 {
     public static Map<String, String> list;
     public static Map<String, String> workspaceID;
     List<String> jsonResponse;
+    String workSpaceId;
+    String userId;
 
     @BeforeClass
-    public void setup() {
+    public String getAuthToken() {
         RestAssured.baseURI = "https://api.octoperf.com:443/public/users";
         path = "/login";
-
-
+        
         HashMap<String, Object> map = new HashMap<String, Object>();
         map.put("password", "test12");
         map.put("username", "tla.jiraone@gmail.com");
-
-        response = RestAssured.given()
+        
+        return RestAssured.given()
                 .queryParams(map)
-                .when()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .baseUri(RestAssured.baseURI)
                 .post(path)
                 .then()
-                .contentType(ContentType.JSON)
-                .extract().response();
-        //JsonPath is an alternative to using XPath for easily getting values from a JSON document
-        JsonPath jsonpathEvaluator = response.jsonPath();
-        String token = jsonpathEvaluator.get("token").toString();
-
-        // Help us Visualize TOKEN
-        System.out.println(jsonpathEvaluator.get("token").toString());
-
-        //Store in a MAP key value response for the token so it can be used in many requests
-        list = new HashMap<String, String>();
-        list.put("Authorization", token);
-        System.out.println("====>"+list);
-        System.out.println("Log in Successfully");
-        System.out.println("======== Test Started ========");
+                .statusCode(SC_OK)
+                .extract()
+                .body()
+                .jsonPath()
+                .get("token");
     }
 
     @Test
     public void validateResponseBody() {
         RestAssured.baseURI = "https://api.octoperf.com";
         response = RestAssured.given()
-                .headers(list)
+                .header("Authorization",  getAuthToken())
                 .when()
                 .get("/workspaces/member-of")
-                .then()
+                .then().log().all()
                 .extract().response();
 
         Assert.assertEquals(200, response.statusCode());
@@ -76,15 +69,15 @@ public class getWorkspaceMemberByWorkspace_05 {
 
         //we have 1 array with 2 Json response body
         jsonResponse = response.jsonPath().getList("id");
-        System.out.println(jsonResponse.get(1));
+        System.out.println(jsonResponse.get(0));
     }
 
     @Test(dependsOnMethods={"validateResponseBody"})
     public void validateResponseBody2() {
-        String workspaceId = jsonResponse.get(1);
+        String workspaceId = jsonResponse.get(0);
         RestAssured.baseURI = "https://api.octoperf.com";
         response = RestAssured.given()
-                .headers(list)
+                .header("Authorization",  getAuthToken())
                 .when()
                 .get("workspaces/members/by-workspace/"+workspaceId)
                 .then()
@@ -94,10 +87,10 @@ public class getWorkspaceMemberByWorkspace_05 {
         System.out.println(response.prettyPrint());
 
         JsonPath jsonpathEvaluator = response.jsonPath();
-        String userId = jsonpathEvaluator.get("userId").toString();
+        userId = jsonpathEvaluator.get("userId").toString();
         System.out.println("_---->"+ userId);
 
-        String workSpaceId = jsonpathEvaluator.get("workspaceId").toString();
+        workSpaceId = jsonpathEvaluator.get("workspaceId").toString();
         System.out.println("_---->"+ workSpaceId);
 
         workspaceID = new HashMap<String, String>();
@@ -107,19 +100,18 @@ public class getWorkspaceMemberByWorkspace_05 {
         System.out.println("_->"+ workspaceID.get("userId"));
         System.out.println("_->"+ workspaceID.get("workspaceId"));
 
-
     }
 
 
     @Test(dependsOnMethods ={"validateResponseBody2"} )
     public void validatecreateProject() {
-        String requestBody = "{\"id\":\"\",\"created\":\"2021-03-11T06:15:20.845Z\",\"lastModified\":\"2021-03-11T06:15:20.845Z\",\"userId\":\""+workspaceID.get("userId")+"\",\"workspaceId\":\""+workspaceID.get("workspaceId")+"\",\"name\":\"testing22\",\"description\":\"testing\",\"type\":\"DESIGN\",\"tags\":[]}";
-
-        String workspaceId = jsonResponse.get(1);
+        String requestBody = "{\"id\":\"\",\"created\":\"2021-03-11T06:15:20.845Z\",\"lastModified\":\"2021-03-11T06:15:20.845Z\",\"userId\":\""+workSpaceId+"\",\"workspaceId\":\""+userId+"\",\"name\":\"testing22\",\"description\":\"testing\",\"type\":\"DESIGN\",\"tags\":[]}";
+        System.out.println(requestBody);
+        String workspaceId = jsonResponse.get(0);
         RestAssured.baseURI = "https://api.octoperf.com";
         response = RestAssured.given()
                 .headers("Content-type", "application/json")
-                .headers("Authorization", "834f9392-ce89-411c-8366-00c86886ec9b")
+                .header("Authorization",  getAuthToken())
                 .and()
                 .body(requestBody)
                 .when()
@@ -127,9 +119,8 @@ public class getWorkspaceMemberByWorkspace_05 {
                 .then()
                 .extract().response();
         System.out.println(response.prettyPrint());
-        System.out.println(response.prettyPeek());
+        System.out.println(response.getStatusLine());
 
     }
-
 
 }

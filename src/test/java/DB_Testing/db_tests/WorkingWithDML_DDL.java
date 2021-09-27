2424fbc98167a3fe2e470a019fd0f23f854ed17a
ninja <n1njat3k@hotmail.com>
2021-09-27 18:16:06 -0400

    some data

--- src/test/java/DB_Testing/db_tests/WorkingWithDML_DDL.java
+++ src/test/java/DB_Testing/db_tests/WorkingWithDML_DDL.java
@@ -8,7 +8,7 @@ import java.sql.*;
 import java.util.List;
 import java.util.Map;
 
-public class UsingDMLCommads {
+public class WorkingWithDML_DDL {
     public static void main(String... args) throws SQLException, ClassNotFoundException {
 //        createTable();
 //        InsertToDB();
